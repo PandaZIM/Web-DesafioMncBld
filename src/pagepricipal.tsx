2e34b3a78e3320bb3e01f3ce7f003526b9eb8cb8
pandazim <jaotoledo74@gmail.com>
2020-09-10 20:29:14 -0400

    Criado page pageprincipal

---
+++ src/pagepricipal.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './pageprincipal.sass'
+// import { Container } from './styles';
+
+const PagePrincipal: React.FC = () => {
+  return();
+}
+
+export default PagePrincipal;
