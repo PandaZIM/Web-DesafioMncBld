e9c50f189ee1f07f7f30a732be37bfa0872c76bc
pandazim <jaotoledo74@gmail.com>
2020-09-15 19:54:08 -0400

    Configurado Styles globais

--- src/pagepricipal.tsx
+++ src/pagepricipal.tsx
@@ -4,7 +4,19 @@ import './pageprincipal.sass'
 // import { Container } from './styles';
 
 const PagePrincipal: React.FC = () => {
-  return();
+  return(
+    <div id="page-principal" className=".container">
+        <div id="page-principal-conteudo">
+            <form action="">
+                <fieldset>
+                    <legend>Insira os dados</legend>
+
+                    <input type="text"/>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+  );
 }
 
 export default PagePrincipal;
