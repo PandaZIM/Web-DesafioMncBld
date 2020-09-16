b463de194c72b4120d8a8057c60e551ee953fa61
pandazim <jaotoledo74@gmail.com>
2020-09-15 20:23:33 -0400

    Criado botões com seus respectivos states

--- src/pagepricipal.tsx
+++ src/pagepricipal.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './pageprincipal.sass'
 // import { Container } from './styles';
 
 const PagePrincipal: React.FC = () => {
+
+  const [userName, setUserName] = useState('');
+  const [userLastName, setUserLastName] = useState('')
+  const [userBirthday, setUserBirthday] = useState('')
+  const [userGender, setUserGender] = useState('')
+  const [userCPF, setUserCPF] = useState('')
+
+  const submitUserInformation = () => {
+    const userInformation = {
+      'Name' : userName,
+      'Last Name' : userLastName,
+      'Birthday': userBirthday,
+      'Gender': userGender,
+      'CPF': userCPF
+    }
+  }
   return(
     <div id="page-principal" className=".container">
         <div id="page-principal-conteudo">
             <form action="">
+            <legend>Insira os dados</legend>
                 <fieldset>
-                    <legend>Insira os dados</legend>
-
-                    <input type="text"/>
+                    
+                    <input type="text" 
+                    placeholder="Name"
+                    onChange={e => setUserName(e.target.value)}/>
+                    <input type="text"
+                    placeholder="Last Name"
+                    onChange={e => setUserLastName(e.target.value)}/>
+                    <input type="text"
+                    placeholder="Birthday"
+                    onChange={e => setUserBirthday(e.target.value)}/>
+                    <input type="text"
+                    placeholder="Gender"
+                    onChange={e => setUserGender(e.target.value)}/>
+                    <input type="text"
+                    placeholder="CPF"
+                    onChange={e => setUserCPF(e.target.value)}/>
+                    
                 </fieldset>
             </form>
         </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import PagePrincipal from './pagepricipal'
 
 function Routes () {
     return(
